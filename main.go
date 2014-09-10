98bb32b1f7dd97d45b5d4097087ec605a9ec277a
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2014-09-10 11:26:55 -0700

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal([]string{"San Francisco", "Amsterdam",
+	"Berlin","Palo Alto"})
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(data)
+}
+
+func main() {
+	http.HandleFunc("/", HomeHandler)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
