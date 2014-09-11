5260bc1e0e04a8f230c53900d248c417bbb854b4
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2014-09-10 18:36:05 -0700

    broke

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	data, _ := json.Marshal([]string{"San Francisco", "Amsterdam",
-	"Berlin","Palo Alto", "Los Altos"})
+	"Berlin","Palo Alto", "Los Altos})
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Write(data)
 }
